5aadada69d167e49bb4b050c46d1d1fb5a2cc9b3
sonelin09 <sonelin0309@gmail.com>
2022-06-29 12:59:47 +0800

    完成按數字鍵顯示到label

--- main.js
+++ main.js
@@ -18,4 +18,22 @@ function operate(a, b) {
     return add(a, b);
 }
 
+//按下數字鍵要執行的
+let num = [];
+const monitor = document.getElementsByClassName("monitor");
+function populate() {
+    num.push(Number(this.textContent));
+    console.log(num);
+    //陣列轉字串+去逗點後，顯示於label
+    let temp = num.join().replace(/,/g, "");
+    console.log(temp);
+    monitor[0].textContent = temp;
+    //monitor.textContent = toString(num);
+    
+}
+
+const digit = document.getElementsByClassName("digit");
+for(let i=0; i<digit.length; i++) {
+    digit[i].addEventListener('click', populate);
+}
 
