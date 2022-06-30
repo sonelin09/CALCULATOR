dc79ca601f5db17c9e1c31135ed3aa5a84aa6e9f
sonelin09 <sonelin0309@gmail.com>
2022-06-30 13:11:32 +0800

    寫clear鍵初步功能

--- main.js
+++ main.js
@@ -20,12 +20,13 @@ function operate(a, b) {
 
 //按下數字鍵要執行的
 let num = [];
+let temp;
 const monitor = document.getElementsByClassName("monitor");
 function populate() {
     num.push(Number(this.textContent));
     console.log(num);
     //陣列轉字串+去逗點後，顯示於label
-    let temp = num.join().replace(/,/g, "");
+    temp = num.join().replace(/,/g, "");
     console.log(temp);
     monitor[0].textContent = temp;
     //monitor.textContent = toString(num);
@@ -34,6 +35,14 @@ function populate() {
 
 const digit = document.getElementsByClassName("digit");
 for(let i=0; i<digit.length; i++) {
-    digit[i].addEventListener('click', populate);
+    digit[i].addEventListener("click", populate);
+}
+
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", clear);
+function clear() {
+    monitor[0].textContent = '0';
+    num = [];
+    temp = "";
 }
 
