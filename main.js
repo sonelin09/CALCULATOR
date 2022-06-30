bc2c0960747d8758cb5a47e3e54f95683fc9d824
sonelin09 <sonelin0309@gmail.com>
2022-06-30 13:45:46 +0800

    可存取+-x/並顯示在畫面

--- main.js
+++ main.js
@@ -18,6 +18,12 @@ function operate(a, b) {
     return add(a, b);
 }
 
+//數字鍵
+const digit = document.getElementsByClassName("digit");
+for(let i=0; i<digit.length; i++) {
+    digit[i].addEventListener("click", populate);
+}
+
 //按下數字鍵要執行的
 let num = [];
 let temp;
@@ -33,11 +39,38 @@ function populate() {
     
 }
 
-const digit = document.getElementsByClassName("digit");
-for(let i=0; i<digit.length; i++) {
-    digit[i].addEventListener("click", populate);
+//運算符
+const operators = document.getElementsByClassName("operator");
+for(let j = 0; j < operators.length; j++) {
+    operators[j].addEventListener("click", operate);
 }
 
+let operator = "";
+//第一個數
+let num1;
+function storeClear() {
+    num1 = Number(temp);
+    num = [];
+    temp = "";
+    monitor[0].textContent = `${num1} ${operator}`;
+}
+function operate() {
+    if(this.textContent === "+") {
+        operator = "+";
+        storeClear();
+    }else if(this.textContent === "-") {
+        operator = "-";
+        storeClear();
+    }else if(this.textContent === "x") {
+        operator = "x";
+        storeClear();
+    }else if(this.textContent === "/") {
+        operator = "/";
+        storeClear();
+    }
+}
+
+//清除鍵
 const clearBtn = document.getElementById("clear");
 clearBtn.addEventListener("click", clear);
 function clear() {
