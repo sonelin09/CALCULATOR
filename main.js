e276473bb276de240b95b7a9cf6719842db7da1d
sonelin09 <sonelin0309@gmail.com>
2022-07-03 21:37:15 +0800

    結果取四捨五入到小數點後第五位 並去除零

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a-b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(a, b) {
-    return add(a, b);
-}
-
 //數字鍵
 const digit = document.getElementsByClassName("digit");
 for(let i=0; i<digit.length; i++) {
@@ -29,15 +9,32 @@ let num = [];
 let temp;
 const monitor = document.getElementById("monitor1");
 function populate() {
-    num.push(Number(this.textContent));
+    if(operator.length === 0 ) {
+        addNumber(this);
+    }else {
+        if(count === 0) {
+            storeClear(); 
+            addNumber(this);
+        }else {
+            addNumber(this);
+        }
+        
+    }
+    
+    
+}
+
+//紀錄數字
+function addNumber(nowNum) {
+    num.push(Number(nowNum.textContent));
     console.log(num);
     //陣列轉字串+去逗點後，顯示於label
     temp = num.join().replace(/,/g, "");
     console.log(temp);
     monitor.textContent = temp;
     //monitor.textContent = toString(num);
-    
 }
+
 
 //運算符
 const operators = document.getElementsByClassName("operator");
@@ -48,17 +45,21 @@ for(let j = 0; j < operators.length; j++) {
 let operator = [];
 let numWithOperator = [];
 //第一個數
-let num1;
+let num1 = 0;
+let count = 0;
 //把現有數值紀錄後清除
 function storeClear() {
     num1 = Number(temp);
     num = [];
     temp = "";
-    monitor.textContent = `${num1} ${operator}`;
+    monitor.textContent = "0";
+    count ++;
 }
+
+//用來儲存目前的運算子
 function operate() {
     operator.push(this.textContent);
-    monitor.textContent = `${temp} ${operator}`;
+    monitor2.textContent = `${temp} ${operator}`;
     console.log(operator);
 }
 
@@ -69,12 +70,17 @@ function clear() {
     monitor.textContent = '0';
     num = [];
     temp = "";
+    num1 = 0;
+    operator = [];
+    count = 0;
+    monitor2.textContent = "";
 }
 
 //倒退鍵
 const backSpace = document.getElementById("backspace");
 backSpace.addEventListener("click", back);
 function back() {
+    
     num.splice(num.length - 1, 1);
     console.log(num);
     temp = num.join().replace(/,/g, "");
@@ -82,18 +88,54 @@ function back() {
     monitor.textContent = temp;
 }
 
+//儲存第二個數
+let num2;
+//儲存計算結果
+let num3;
+
 //等於鍵
-let equal =document.getElementById("equal");
+let equal = document.getElementById("equal");
 equal.addEventListener("click", outcome);
-/*
+//計算結果
 function outcome() {
-    switch(operator) {
-        case "+":
-            num2 = num1 + num2
-            break;
-        case "-":
-            num2 = num1 + num2
-            break;
+    /*
+    console.log(`operator: ${operator}`);
+    console.log(`num2: ${num2}`);
+    */
+    if(isNaN(temp) === false) {
+        num2 = Number(temp);
+        switch(operator[0]) {
+            case "+":
+                num3 = parseFloat((num1 + num2).toFixed(5)) 
+                monitor.textContent = `${num3}`
+                allClear()
+                break;
+            case "-":
+                num3 = parseFloat((num1 - num2).toFixed(5)) 
+                monitor.textContent = `${num3}`
+                allClear()
+                break;
+            case "x":
+                num3 = parseFloat((num1 * num2).toFixed(5)) 
+                monitor.textContent = `${num3}`
+                allClear()
+                break;
+            case "/":
+                num3 = parseFloat((num1 / num2).toFixed(5)) 
+                monitor.textContent = `${num3}`
+                allClear()
+                break;
+        }
     }
 }
-*/
+
+//歸零
+function allClear() {
+    num = [];
+    temp = "";
+    num1 = 0;
+    num2 = 0;
+    operator = [];
+    count = 0;
+    monitor2.textContent = "";
+}
